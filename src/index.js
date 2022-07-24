775d1e1be46a7c3303dd105720bd835b011bb807
Lewis Birch <l.birch@lancaster.ac.uk>
2022-07-24 18:22:24 +0100

    Changed png's to webp's.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function importAll(r) {
     return r.keys().map(r);
 }
 
-const images = importAll(require.context('./assets/images', false, /\.(png|jpe?g|svg)$/));
+const images = importAll(require.context('./assets/images', false, /\.(webp)$/));
 
 console.log(images)
 
